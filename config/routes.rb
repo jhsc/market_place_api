d8ebc1d0833237c10f46a2473f7645c324620c5f
Juan Santa Cruz <jhsantacruz@icloud.com>
2016-03-29 23:03:11 -0700

    Set the api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
-    constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1 do
+      # We are going to list our resources here
+    end
   end
 end

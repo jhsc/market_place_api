76a9501ed0488ed110c1c00d916fbc123ba6eac7
Juan Santa Cruz <jhsantacruz@icloud.com>
2016-04-04 00:42:54 -0700

    Cleaned a bit User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,15 @@ RSpec.describe User, type: :model do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:auth_token) }
 
-  it { should be_valid }
-
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   it { should validate_uniqueness_of(:auth_token)}
+
+  it { should have_many(:products) }
+
+  it { should be_valid }
 
   describe "#generate_authentication_token!" do
     it "generates a unique token" do
@@ -32,10 +34,7 @@ RSpec.describe User, type: :model do
     end
   end
 
-  it { should have_many(:products) }
-  
   describe "#products association" do
-
     before do
       @user.save
       3.times { FactoryGirl.create :product, user: @user }
